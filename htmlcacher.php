794e98f791c4d8ae7fa39c60539ca88172e2ce04
Mark Tanasevych <mark.tanasevych@academysmart.com>
2023-11-27 19:56:33 +0200

    clear

--- htmlcacher.php
+++ htmlcacher.php
@@ -9,31 +9,19 @@
     License: GPL3
 */
 class MainHtmlCacher{
-	private $templates = array(
-		'404',
-		'archive',
-		'attachment',
-		'author',
-		'category',
-		'date',
-		'embed',
-		'frontpage',
-		'home',
-		'index',
-		'page',
-		'paged',
-		'privacypolicy',
-		'search',
-		'single',
-		'singular',
-		'tag',
-		'taxonomy',
-	);
+	private $templates = array('404','archive','attachment','author','category','date','embed','frontpage','home','index','page','paged','privacypolicy','search','single','singular','tag','taxonomy');
 	public function __construct(){
+		register_activation_hook(__FILE__,array($this,'activate'));
 		if(!is_admin()){
 			foreach ($this->templates as $type) {
 				add_filter( "{$type}_template", array($this,'template_hierarchy'),10,3 );
 			}
+		}
+	}
+	public function activate(){
+		$upload_dir   = wp_upload_dir();
+		if(!is_dir($upload_dir['basedir'].'/htmlcache')){
+			mkdir($upload_dir['basedir'].'/htmlcache');
 		}
 	}
 	public function template_hierarchy($template, $type, $templates){
@@ -42,7 +30,8 @@ class MainHtmlCacher{
 			$template_path = explode('/',$template);
 			$template_name = $template_path[count($template_path)-1];
 			$template_name = str_replace('.php','.html',$template_name);
-			$file_cache_path = __DIR__.'/cache/'.$template_name;
+			$upload_dir   = wp_upload_dir();
+			$file_cache_path = $upload_dir['basedir'].'/htmlcache/'.$template_name;
 			if(!file_exists($file_cache_path)){
 				$f = fopen($file_cache_path,'w+');
 				ob_start();
